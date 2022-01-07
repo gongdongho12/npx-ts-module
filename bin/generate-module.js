8d34582d7d796ceb5e1302ce615f032546e96921
Dongho Gang <gongdongho12@gmail.com>
2022-01-07 11:28:47 +0900

    fix: change repository

--- bin/generate-module.js
+++ bin/generate-module.js
@@ -14,7 +14,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = "https://github.com/gongdongho12/npx-ts-module";
+const GIT_REPO = "https://github.com/gongdongho12/typescript-npm-module";
 
 if (projectName !== ".") {
 	try {
